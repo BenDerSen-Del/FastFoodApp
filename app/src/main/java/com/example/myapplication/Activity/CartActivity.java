7b3ea67ad476165b57e3aafd2c4216c9ac231b01
Dell <duongmisugijax123@gmail.com>
2024-06-26 23:20:09 +0700

    sâsjaskasjjska

--- app/src/main/java/com/example/myapplication/Activity/CartActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/CartActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.R;
+
 public class CartActivity extends AppCompatActivity {
 
     @Override
